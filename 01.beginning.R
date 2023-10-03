6e244249d8f051d5f817aabfa3049b294fe4489d
asiacotrufo <146853999+asiacotrufo@users.noreply.github.com>
2023-10-03 17:55:23 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -8,3 +8,10 @@ asia <- 3 + 5
 #showing results
 asia
 
+#arrays
+sophi <- c(10, 20, 30, 50, 70) #microplastics
+# function(argument1, argument2, argument3)
+paula <- c(100, 500, 600, 1000, 2000) #people
+plot(paula, sophi, xlab = "number of people", ylab="percentage of microplastic", pch= 11, cex=2, col="red")
+#or you can assign paula and sophi to new objects
+
