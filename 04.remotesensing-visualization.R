e2ed7786e5310e16b3b35126277902565668d7a8
asiacotrufo <146853999+asiacotrufo@users.noreply.github.com>
2023-11-02 17:33:27 +0100

    Update 04.remotesensing-visualization.R

--- 04.remotesensing-visualization.R
+++ 04.remotesensing-visualization.R
@@ -20,4 +20,22 @@ b4
 b8 <- im.import("sentinel.dolomites.b8.tif") # b2 is the green wavelength
 b8
 
+#stack images
+stacksent <- c(b2,b3,b4,b8)
+dev.off() #it closes devices
+plot(stacksent, col=cl)
+
+plot(stacksent[[4]], col=cl)
+
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+clg <- colorRampPalette(c("dark green", "green", "light green")) (100)
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+cln <- colorRampPalette(c("brown", "orange", "yellow")) (100)
+
+par(mfrow=c(2,2))
+plot(b2, col=clb)
+plot(b3, col=clg)
+plot(b4, col=clr)
+plot(b8, col=cln)
+
 
